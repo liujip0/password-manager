53b9d1e64a495ef8b3d4130dc2850a06ec72eed1
liujip0 <liujip2020@gmail.com>
2025-12-28 03:39:56 -0500

    make parsing input args work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let command = parse_args(&args);
+    println!("{:?}", command);
+}
+
+#[derive(Debug)]
+enum Command {
+    None,
+    Invalid(String),
+
+    List,
+    Get {
+        key: String,
+    },
+    Set {
+        key: String,
+        value: String,
+    },
+    Generate {
+        key: String,
+        special_chars: bool,
+        length: usize,
+    },
+}
+
+fn parse_args(args: &Vec<String>) -> Command {
+    let command = match args.get(1) {
+        Some(command) => command,
+        None => return Command::None,
+    };
+
+    match command.as_str() {
+        "list" => Command::List,
+        "get" => {
+            let key = match args.get(2) {
+                Some(key) => key,
+                None => return Command::Invalid("No key provided.".to_string()),
+            };
+            Command::Get { key: key.clone() }
+        }
+        "set" => {
+            let key = match args.get(2) {
+                Some(key) => key,
+                None => return Command::Invalid("No key provided.".to_string()),
+            };
+            let value = match args.get(3) {
+                Some(value) => value,
+                None => return Command::Invalid("No value provided.".to_string()),
+            };
+            Command::Set {
+                key: key.clone(),
+                value: value.clone(),
+            }
+        }
+        "generate" => {
+            let key = match args.get(2) {
+                Some(key) => key,
+                None => return Command::Invalid("No key provided.".to_string()),
+            };
+            let special_chars = true;
+            let length = 32;
+            Command::Generate {
+                key: key.clone(),
+                special_chars,
+                length,
+            }
+        }
+        _ => Command::Invalid(format!("Unknown command: {}", command)),
+    }
 }
