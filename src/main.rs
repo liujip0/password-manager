d54e3c66aabc6a2ee55a68811375a057937d4e18
liujip0 <liujip2020@gmail.com>
2025-12-28 04:41:52 -0500

    use clap for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,14 @@
-use std::env;
+use clap::{ArgAction, Parser, Subcommand};
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let command = parse_args(&args);
-    println!("{:?}", command);
+#[derive(Parser)]
+#[command(version, about, author)]
+struct CLI {
+    #[command(subcommand)]
+    command: Commands,
 }
 
-#[derive(Debug)]
-enum Command {
-    None,
-    Invalid(String),
-
+#[derive(Debug, Subcommand)]
+enum Commands {
     List,
     Get {
         key: String,
@@ -21,53 +19,14 @@ enum Command {
     },
     Generate {
         key: String,
+        #[arg(short, long = "special", default_value_t = true, action = ArgAction::SetFalse)]
         special_chars: bool,
+        #[arg(short, long, default_value_t = 32)]
         length: usize,
     },
 }
 
-fn parse_args(args: &Vec<String>) -> Command {
-    let command = match args.get(1) {
-        Some(command) => command,
-        None => return Command::None,
-    };
-
-    match command.as_str() {
-        "list" => Command::List,
-        "get" => {
-            let key = match args.get(2) {
-                Some(key) => key,
-                None => return Command::Invalid("No key provided.".to_string()),
-            };
-            Command::Get { key: key.clone() }
-        }
-        "set" => {
-            let key = match args.get(2) {
-                Some(key) => key,
-                None => return Command::Invalid("No key provided.".to_string()),
-            };
-            let value = match args.get(3) {
-                Some(value) => value,
-                None => return Command::Invalid("No value provided.".to_string()),
-            };
-            Command::Set {
-                key: key.clone(),
-                value: value.clone(),
-            }
-        }
-        "generate" => {
-            let key = match args.get(2) {
-                Some(key) => key,
-                None => return Command::Invalid("No key provided.".to_string()),
-            };
-            let special_chars = true;
-            let length = 32;
-            Command::Generate {
-                key: key.clone(),
-                special_chars,
-                length,
-            }
-        }
-        _ => Command::Invalid(format!("Unknown command: {}", command)),
-    }
+fn main() {
+    let cli = CLI::parse();
+    println!("Action: {:?}", cli.command);
 }
