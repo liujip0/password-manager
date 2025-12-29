d5e392ef560a599ee737eb98607ebe21cae5fcc5
liujip0 <liujip2020@gmail.com>
2025-12-29 10:10:19 -0500

    make encrypt work by xor

--- src/password.rs
+++ src/password.rs
@@ -1,9 +1,33 @@
 pub fn encrypt(password: &str, master_password: &str) -> Result<String, String> {
-    // Placeholder for encryption logic
-    Ok(password.to_string()) // Replace with actual encrypted password
+    let password = password.as_bytes();
+    let master_password = master_password.as_bytes();
+
+    let encrypted_password: Vec<u8> = password
+        .iter()
+        .enumerate()
+        .map(|i| i.1 ^ master_password[i.0 % master_password.len()])
+        .collect();
+
+    let encrypted_password = String::from_utf8(encrypted_password);
+    match encrypted_password {
+        Err(e) => Err(format!("Could not encrypt password.\n\n{}", e)),
+        Ok(ep) => Ok(ep),
+    }
 }
 
 pub fn decrypt(encrypted_password: &str, master_password: &str) -> Result<String, String> {
-    // Placeholder for decryption logic
-    Ok(encrypted_password.to_string()) // Replace with actual decrypted password
+    let encrypted_password = encrypted_password.as_bytes();
+    let master_password = master_password.as_bytes();
+
+    let decrypted_password: Vec<u8> = encrypted_password
+        .iter()
+        .enumerate()
+        .map(|i| i.1 ^ master_password[i.0 % master_password.len()])
+        .collect();
+
+    let decrypted_password = String::from_utf8(decrypted_password);
+    match decrypted_password {
+        Err(e) => Err(format!("Could not decrypt password.\n\n{}", e)),
+        Ok(dp) => Ok(dp),
+    }
 }
